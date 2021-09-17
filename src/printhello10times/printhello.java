9933c45f7eb85199acd564b8f4d16de0b8691902
Ankit <Ankit@Ankit-PC>
2021-09-17 20:41:02 +0530

    hello 10 times

---
+++ src/printhello10times/printhello.java
@@ -0,0 +1,15 @@
+package printhello10times;
+import java.util.*;
+
+public class printhello {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
